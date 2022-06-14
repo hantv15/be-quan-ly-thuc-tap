37a139ea8c8b82ac9bf324e5f76fd5409bddcf84
hantv15 <hantvph10714@fpt.edu.vn>
2022-06-14 21:17:21 +0700

    fix be

--- src/middlewares/CheckAuth.js
+++ src/middlewares/CheckAuth.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const { OAuth2Client } = require("google-auth-library");
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
-export const isAuthenticateUser = async (req, res, next) => {
+export const isAuthenticateManager = async (req, res, next) => {
   try {
     const authHeader = req.header("Authorization");
     const accessToken = authHeader && authHeader.split(" ")[1];
@@ -26,16 +26,40 @@ export const isAuthenticateUser = async (req, res, next) => {
       req.role = manager.role;
       next();
     }
-    if (student) {
-      req.role = "student";
-      next();
-    }
   } catch (error) {
     res.status(401).json({
       message: "Unauthorized the server responded with a status of 401",
     });
   }
 };
+
+// export const isAuthenticateManager = async (req, res, next) => {
+//   try {
+//     const authHeader = req.header("Authorization");
+//     const accessToken = authHeader && authHeader.split(" ")[1];
+//     if (!accessToken) {
+//       return res.status(401).json({ message: "Vui lòng đăng nhập tài khoản" });
+//     }
+//     const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+//     const manager = await Manager.findOne({
+//       _id: decoded.userId,
+//       campus_id: decoded.campusId,
+//     });
+//     const student = await Student.findOne({
+//       _id: decoded.userId,
+//       campus_id: decoded.campusId,
+//     });
+
+//     if (manager) {
+//       req.role = manager.role;
+//       next();
+//     }
+//   } catch (error) {
+//     res.status(401).json({
+//       message: "Unauthorized the server responded with a status of 401",
+//     });
+//   }
+// };
 
 export const authorizeRoles = (roles) => {
   return (req, res, next) => {
